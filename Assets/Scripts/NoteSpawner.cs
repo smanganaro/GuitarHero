510cf8a048e2ac97a07434f3ef1b97bf7a884551
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2019-12-06 17:01:19 -0300

    Starting

---
+++ Assets/Scripts/NoteSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawner : MonoBehaviour
+{
+    public GameObject note;
+
+     void Start()
+    {
+        Instantiate(note, transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private const int MAX_NOTES = 5;
+	private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+    public float timeDecrease;
+    public float minTime;	
+
+    public GameObject[] noteTemplate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            int rand = Random.Range(0, MAX_NOTES);
+            Instantiate(noteTemplate[rand], transform.position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+            if (startTimeBtwSpawns > minTime) {
+                startTimeBtwSpawns -= timeDecrease;
+            }
+        }
+        else {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+}
