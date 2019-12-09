d669af20ff7cad0b8bf6f7b9f5b30e6c35d28ebf
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2019-12-09 13:51:48 -0300

    Making reasonable structure

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+	//public GameObject effect;
+	public KeyCode key;
+	private bool triggerEntered = false;
+   	
+   	private GameObject currentNote;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(triggerEntered){
+        	if(Input.GetKeyDown(key)){
+	        	//Instantiate(effect, transform.position, Quaternion.identity);
+	            Destroy(currentNote);
+        	}	
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+    	Debug.Log("Hello");
+
+    	triggerEntered = true;
+    	currentNote = other.gameObject;
+
+    }
+
+    private void OnTriggerExit2D(){
+    	triggerEntered = false;
+    	currentNote = null;
+    }
+}
--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -4,35 +4,14 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
-   	public KeyCode key;
-   	//public GameObject effect;
+
    	public float speed;
-   	public string myTag;
-   	public bool triggerEntered = false;
+   	
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.left * speed * Time.deltaTime);
 
-        if(triggerEntered){
-        	if(Input.GetKeyDown(key)){
-	        	//Instantiate(effect, transform.position, Quaternion.identity);
-	            Destroy(gameObject);
-        	}	
-        }
-        
-    }
-
-   	private void OnTriggerEnter2D(Collider2D other)
-    {
-    	Debug.Log("Hello");
-
-    	triggerEntered = true;
-
-    }
-
-    private void OnTriggerExit2D(){
-    	triggerEntered = false;
     }
     
 }
